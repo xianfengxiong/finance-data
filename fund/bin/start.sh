0e226892e9d1c9f26348c3a6fe0c72a437881550
andy429006 <xianfeng.xiong@caigin.com>
2017-09-13 10:38:44 +0800

    修改启动脚本

--- fund/bin/start.sh
+++ fund/bin/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 lsof -i:8000 | grep java | awk '{print $2}' | xargs kill -9
-nohup java -jar target/finance-fund-1.0.jar >> /home/xxf/log/fund/fund.out
+nohup java -jar target/finance-fund-1.0.jar >> /home/xxf/log/fund/fund.out &
